bf845dba67522b9ad5280421d9be1c1c0a136b3e
Gwyndolyn Marchant <luna@shadenexus.com>
2022-04-25 19:15:22 -0400

    raze support

--- README.md
+++ README.md
@@ -1,33 +1,35 @@
-## gzdoom-discordrpc
-
+## zgame-discordrpc
 ![license](https://img.shields.io/badge/license-public%20domain-green)
 
-A discordrpc client for ZDoom written in Rust.
+A discordrpc client for ZDoom and Raze written in Rust.
 
-WIP(?) cross-platform support. It works on Linux and Windows, still need to test on MacOS. Use at own risk and all that yada yada.
+Possibly cross-platform support? It works on Windows. I haven't personally tested it otherwise.
 
-I'm making this because:
+I forked this because:
 
-1. I could not find a pre-existing DiscordRPC client for it
-2. This is something I would actively use, which would make developing it that much more fun
-3. I can actually visualize how I would go about it, unlike most other projects where I feel blind as a bat
+1. DiscordRPC for Raze and GZDoom is still WIP
+2. I play a lot of WADs for friends in Discord.
+3. This was a fun way to learn Rust.
 
 ## How it works
-
 1. Program reads first argument
 2. Program connects to Discord via RPC
-3. It then looks for a doom process based on supplied argument, and parses it's window title ("level - game/mod")
+3. It then looks for a zgame process based on supplied argument, and parses it's window title ("level - game/mod")
 4. It is separated into an &str vector
-5. The icon will be the logo for the game/mod, the status will be the level, and the hover text will be the game/mod
+5. The icon will be the logo for the game/mod, the status will be the game and level, and the hover text will be the engine
 6. Program loops every 15 secs, looping every second would be overkill
 
-## Running
+### Supported Engines
+- GZDoom
+- LZDoom
+- Raze
 
-1. First make sure that GZDoom or LZDoom is running, if using GZDoom make sure to be inside of a level.
+## Running
+1. First make sure that a supported engine is running.
 2. Download a prebuilt binary from the releases section.
 3. Open a terminal (on Windows, CMD or PowerShell should work just fine) then...
 
-ENGINE = `gzdoom` or `lzdoom`
+ENGINE = `gzdoom` or `lzdoom` or `raze`
 
 Linux: `chmod +x gzdoom-discordrpc`, then `./gzdoom-discordrpc ENGINE`
 
@@ -55,7 +57,3 @@ DOOM II:
 Project Brutality:
 
 ![Project Brutality](images/pb.png?raw=true "Project Brutality")
-
-## TODO
-
-1. Rename project to zdoom-discordrpc, that way it makes more sense.
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use discord_rpc_client::Client;
 use fancy_regex::Regex;
 use std::{env, thread, time};
 use window_titles::{Connection, ConnectionTrait};
+use std::collections::HashMap;
 
 // Returns the window title; for gzdoom this is "level - game", for lzdoom this is just "game"
 fn info(connection: &Connection) -> Result<String, Box<dyn std::error::Error>> {
     // List of windows as vector with strings
     let windows: Vec<String> = connection.window_titles().unwrap();
-    let re = Regex::new("((?!(.*Firefox.*)|(.*Pale Moon.*))(DOOM)|(Project Brutality)|(Snap the Sentinel))").unwrap();
+    let re = Regex::new("((?!(.*Firefox.*)|(.*Pale Moon.*))(DOOM)|(Project Brutality)|(Duke))").unwrap();
     let window: String = windows.into_iter().filter(|s| re.is_match(s).unwrap()).collect();
     Ok(window)
 }
@@ -16,9 +17,9 @@ fn main() {
     // Obtain engine from first arg since it would be near impossible to find correct one from the code
     let engine = env::args()
         .nth(1)
-        .expect("Requires an engine to be input (gzdoom/lzdoom)");
+        .expect("Requires an engine to be input (gzdoom/raze/lzdoom)");
     // Create the client
-    let mut drpc = Client::new(729549945408979065);
+    let mut drpc = Client::new(968250311758192650);
 
     // Start up the client connection, so that we can actually send and receive stuff
     drpc.start();
@@ -26,58 +27,65 @@ fn main() {
     // Create connection so that window list may be obtained
     let connection = Connection::new().unwrap();
 
+    let supported_engines = HashMap::from([
+        ("gzdoom", "GZDoom"),
+        ("lzdoom", "LZDoom"),
+        ("raze", "Raze")
+    ]);
+
+    let eng_str = &engine.as_str().clone();
+
+    if !supported_engines.contains_key(eng_str) {
+         println!("Unsupported engine!");
+    }
+
+    let s_engine = supported_engines[eng_str];
+
     loop {
-        if engine == "gzdoom" {
-            let window = info(&connection).unwrap();
-            // "level - game" => [level, game]
-            let game_vec: Vec<&str> = window.split(" - ").collect();
-            let level = game_vec[0];
-            let game = game_vec[1];
-            let status_game = ["Game: ".to_string(), game.to_string()].concat();
-            println!("Engine: GZDoom\n{}\nLevel: {}\n--------------------", status_game, level);
+        let window = info(&connection).unwrap();
+        let game_vec: Vec<&str> = window.split(" - ").collect();
 
-            // Get the icon
-            let icon = match game {
-                "DOOM Registered" => "doom",
-                "DOOM 2: Hell on Earth" => "doom_ii",
-                "DOOM 2: Unity Edition" => "doom_ii",
-                "The Ultimate DOOM" => "ultimate_doom",
-                "Brutal Doom: Project Brutality" => "pb_icon",
-                "Project Brutality 3.0" => "pb_icon",
-                "Snap the Sentinel" => "sts",
-                _ => "gz",
-            };
+        let mut level = "Menus";
 
-            // Set the engine
-            let engine = "Engine: GZDoom";
-
-            // Set the activity
-            if let Err(why) = drpc.set_activity(|a| {
-                a.details(engine)
-                    .state(status_game)
-                    .assets(|ass| ass.large_image(icon).large_text(level))
-            }) {
-                println!("Failed to set presence: {}", why);
-            }
-        } else if engine == "lzdoom" {
-            let window = info(&connection).unwrap();
-            let status_game = ["Game: ".to_string(), window.to_string()].concat();
-            println!("Engine: LZDoom\n{}\n--------------------", status_game);
-
-            let icon = "lz";
-
-            let engine = "Engine: LZDoom";
-
-            if let Err(why) = drpc.set_activity(|a| {
-                a.details(engine)
-                    .state(status_game)
-                    .assets(|ass| ass.large_image(icon).large_text(&window))
-            }) {
-                println!("Failed to set presence: {}", why);
-            }
-        } else {
-            println!("Invalid engine has been inputted.");
+        if game_vec.len() > 1 {
+            level = game_vec[0];
         }
+
+        let game = game_vec[game_vec.len() - 1];
+        let status_game = ["Game: ".to_string(), game.to_string()].concat();
+
+        println!("{}\n{}\nLevel: {}\n--------------------", s_engine, status_game, level);
+
+        let mut icon = "lz";
+
+        if *eng_str == "gzdoom" {
+            // Get the icon
+            icon = match game {
+                "DOOM Registered" => "doom",
+                "DOOM 2: Hell on Earth" => "doom2",
+                "DOOM 2: Unity Edition" => "doom2",
+                "The Ultimate DOOM" => "doom",
+                _ => "gz"
+            };
+        } else if *eng_str == "raze" {
+            icon = match game {
+                "Duke Nukem 3D" => "duke3d",
+                "Duke Nukem 3D: Atomic Edition" => "duke3d",
+                "BLOOD" => "blood",
+                "BLOOD: Unknown Version" => "blood",
+                _ => "rz"
+            };
+        }
+
+        // Set the activity
+        if let Err(why) = drpc.set_activity(|a| {
+            a.details(game.to_string())
+                .state(level)
+                .assets(|ass| ass.large_image(icon).large_text(s_engine.to_string()))
+        }) {
+            println!("Failed to set presence: {}", why);
+        }
+
         // Loop every 15 seconds
         thread::sleep(time::Duration::from_secs(15));
         println!("program has looped\n------------------");
